64760d256bfd11a566cce372882d98b8c0fc29ae
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-08-29 12:21:58 +0100

    refactor: routes names

--- routes/api.php
+++ routes/api.php
@@ -23,12 +23,40 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Endpoint student
+Route::get('/students ', [ApiStudentsController::class, 'index'])->name('api.student.index');
+Route::post('/students ', [ApiStudentsController::class, 'store'])->name('api.student.store');
+Route::get('/students/{student} ', [ApiStudentsController::class, 'show'])->name('api.student.show');
+Route::put('/students/{student} ', [ApiStudentsController::class, 'update'])->name('api.student.update');
+Route::delete('/students/{student} ', [ApiStudentsController::class, 'destroy'])->name('api.student.delete');
 
-Route::apiResource('/students',ApiStudentsController::class)->name('store','api.students.store');
-Route::apiResource('/teachers', ApiTeacherController::class);
-Route::apiResource('/courses', ApiCourseController::class);
-Route::apiResource('/subjects', ApiSubjectController::class);
-Route::apiResource('/departments', ApiDepartamentController::class);
+// Endpoint teacher
+Route::get('/teachers ', [ApiTeacherController::class, 'index'])->name('api.teacher.index');
+Route::post('/teachers ', [ApiTeacherController::class, 'store'])->name('api.teacher.store');
+Route::get('/teachers/{teacher} ', [ApiTeacherController::class, 'show'])->name('api.teacher.show');
+Route::put('/teachers/{teacher} ', [ApiTeacherController::class, 'update'])->name('api.teacher.update');
+Route::delete('/teachers/{teacher} ', [ApiTeacherController::class, 'destroy'])->name('api.teacher.delete');
+
+// Endpoint Course
+Route::get('/courses ', [ApiCourseController::class, 'index'])->name('api.course.index');
+Route::post('/courses ', [ApiCourseController::class, 'store'])->name('api.course.store');
+Route::get('/courses/{course} ', [ApiCourseController::class, 'show'])->name('api.course.show');
+Route::put('/courses/{course} ', [ApiCourseController::class, 'update'])->name('api.course.update');
+Route::delete('/courses/{course} ', [ApiCourseController::class, 'destroy'])->name('api.course.delete');
+
+// Endpoint Department
+Route::get('/departments ', [ApiDepartamentController::class, 'index'])->name('api.department.index');
+Route::post('/departments ', [ApiDepartamentController::class, 'store'])->name('api.department.store');
+Route::get('/departments/{departments} ', [ApiDepartamentController::class, 'show'])->name('api.department.show');
+Route::put('/departments/{departments} ', [ApiDepartamentController::class, 'update'])->name('api.department.update');
+Route::delete('/departments/{departments} ', [ApiDepartamentController::class, 'destroy'])->name('api.department.delete');
+
+// Endpoint subject
+Route::get('/subjects ', [ApiSubjectController::class, 'index'])->name('api.subject.index');
+Route::post('/subjects ', [ApiSubjectController::class, 'store'])->name('api.subject.store');
+Route::get('/subjects/{subject} ', [ApiSubjectController::class, 'show'])->name('api.subject.show');
+Route::put('/subjects/{subject} ', [ApiSubjectController::class, 'update'])->name('api.subject.update');
+Route::delete('/subjects/{subject} ', [ApiSubjectController::class, 'destroy'])->name('api.subject.delete');
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,43 @@ Route::fallback(function(){
 
 
 Route::prefix('app')->group(function () {
+    // Endpoint Home
     Route::get('/home', [HomeController::class, 'index'])->name('app.home');
-    Route::resource('/students', StudentController::class)->name('index','app.students');
-    Route::resource('/teachers', TeacherController::class)->name('index','app.teachers');
-    Route::resource('/courses', CourseController::class)->name('index','app.courses');
+
+    // Endpoint student
+    Route::get('/students ', [StudentController::class, 'index'])->name('app.student.index');
+    Route::post('/students ', [StudentController::class, 'store'])->name('app.student.store');
+    Route::get('/students/{student} ', [StudentController::class, 'show'])->name('app.student.show');
+    Route::put('/students/{student} ', [StudentController::class, 'update'])->name('app.student.update');
+    Route::delete('/students/{student} ', [StudentController::class, 'destroy'])->name('app.student.delete');
+
+    // Endpoint teacher
+    Route::get('/teachers ', [TeacherController::class, 'index'])->name('app.teacher.index');
+    Route::post('/teachers ', [TeacherController::class, 'store'])->name('app.teacher.store');
+    Route::get('/teachers/{teacher} ', [TeacherController::class, 'show'])->name('app.teacher.show');
+    Route::put('/teachers/{teacher} ', [TeacherController::class, 'update'])->name('app.teacher.update');
+    Route::delete('/teachers/{teacher} ', [TeacherController::class, 'destroy'])->name('app.teacher.delete');
+
+    // Endpoint Course
+    Route::get('/courses ', [CourseController::class, 'index'])->name('app.course.index');
+    Route::post('/courses ', [CourseController::class, 'store'])->name('app.course.store');
+    Route::get('/courses/{course} ', [CourseController::class, 'show'])->name('app.course.show');
+    Route::put('/courses/{course} ', [CourseController::class, 'update'])->name('app.course.update');
+    Route::delete('/courses/{course} ', [CourseController::class, 'destroy'])->name('app.course.delete');
+
+/*     // Endpoint Department
+    Route::get('/departments ', [DepartamentController::class, 'index'])->name('app.department.index');
+    Route::post('/departments ', [DepartamentController::class, 'store'])->name('app.department.store');
+    Route::get('/departments/{departments} ', [DepartamentController::class, 'show'])->name('app.department.show');
+    Route::put('/departments/{departments} ', [DepartamentController::class, 'update'])->name('app.department.update');
+    Route::delete('/departments/{departments} ', [DepartamentController::class, 'destroy'])->name('app.department.delete'); */
+/*
+    // Endpoint subject
+    Route::get('/subjects ', [SubjectController::class, 'index'])->name('app.subject.index');
+    Route::post('/subjects ', [SubjectController::class, 'store'])->name('app.subject.store');
+    Route::get('/subjects/{subject} ', [SubjectController::class, 'show'])->name('app.subject.show');
+    Route::put('/subjects/{subject} ', [SubjectController::class, 'update'])->name('app.subject.update');
+    Route::delete('/subjects/{subject} ', [SubjectController::class, 'destroy'])->name('app.subject.delete'); */
 });
 
 
